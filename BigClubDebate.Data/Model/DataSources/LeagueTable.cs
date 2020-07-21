0434aad4e60408bc5c564f7b215b4bb1447270d0
Chrisb <ccbing@gmail.com>
2020-07-21 02:53:10 +0100

    clean up diagnostic code

--- BigClubDebate.Data/Model/DataSources/LeagueTable.cs
+++ BigClubDebate.Data/Model/DataSources/LeagueTable.cs
@@ -22,7 +22,7 @@ namespace BigClubDebate.Data.Model.DataSources
             return enumerable.ToList();
         }
 
-        public static IOrderedEnumerable<(string, int, List<string>)> TableAndPoints(IEnumerable<Game> games)
+        public static IOrderedEnumerable<(string, int)> TableAndPoints(IEnumerable<Game> games)
         {
             var teams = games
                 .Select(g => g.Home)
@@ -30,28 +30,14 @@ namespace BigClubDebate.Data.Model.DataSources
 
             var st = teams
                 .ToDictionary(teamName => teamName, startCount => 0);
-            var pl = teams
-                .ToDictionary(teamName => teamName, startCount => new List<string>());
-
-            var fails = st.Where(s => games.Count(g => s.Key == g.Home) != games.Count(g => s.Key == g.Away)).ToList();
-            var c = games.Count();
-            //var d = games.Min(x => x.Date);
-            //var e = games.Max(x => x.Date);
-
-            var losers = games.Where(g => g.Loser == "Sheffield Wednesday").ToList();
-            var winner = games.Where(g => g.Winner == "Sheffield Wednesday").ToList();
-            var draw = games.Where(g => g.Drawn && g.Teams.Contains("Sheffield Wednesday")).ToList();
 
             foreach (var game in games)
             {
                 st[game.Home] += game.PointsFor(game.Home);
                 st[game.Away] += game.PointsFor(game.Away);
-                pl[game.Home].Add($"{game.PointsFor(game.Home)}:{game.HomeGoals}-{game.AwayGoals}");
-                pl[game.Away].Add($"{game.PointsFor(game.Away)}:{game.HomeGoals}-{game.AwayGoals}");
-                
             }
 
-            return st.Select(s=>(s.Key,s.Value,pl[s.Key]))
+            return st.Select(s=>(s.Key,s.Value))
                 .OrderByDescending(x => x.Item2);
         }
     }
